77c53907415e5f8c4d0c131861b87336b244b30d
unika00 <112581692+unika00@users.noreply.github.com>
2022-10-24 15:55:27 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,26 @@ import * as d3 from "d3";
 import { barChart } from "./bar-chart";
 import { Int32, Table, Utf8 } from "apache-arrow";
 import { db } from "./duckdb";
-import parquet from "./weather.parquet?url";
+import parquet from "./pittsburgh-air-quality.parquet?url";
 
 const app = document.querySelector("#app")!;
 
 // Create the chart. The specific code here makes some assumptions that may not hold for you.
 const chart = barChart();
 
-async function update(location: string) {
+async function update(City: string) {
   // Query DuckDB for the data we want to visualize.
-  const data: Table<{ weather: Utf8; cnt: Int32 }> = await conn.query(`
-  SELECT weather, count(*)::INT as cnt
-  FROM weather.parquet
-  WHERE location = '${location}'
-  GROUP BY weather
+  const data: Table<{ cnt: Int32; AQI: Utf8 }> = await conn.query(`
+  SELECT AQI, count(*)::INT as cnt
+  FROM pittsburgh-air-quality.parquet
+  WHERE City = '${City}'
+  GROUP BY AQI
   ORDER BY cnt DESC`);
 
   // Get the X and Y columns for the chart. Instead of using Parquet, DuckDB, and Arrow, we could also load data from CSV or JSON directly.
   const X = data.getChild("cnt")!.toArray();
   const Y = data
-    .getChild("weather")!
+    .getChild("AQI")!
     .toJSON()
     .map((d) => `${d}`);
 
@@ -33,7 +33,7 @@ async function update(location: string) {
 // Load a Parquet file and register it with DuckDB. We could request the data from a URL instead.
 const res = await fetch(parquet);
 await db.registerFileBuffer(
-  "weather.parquet",
+  "pittsburgh-air-quality.parquet",
   new Uint8Array(await res.arrayBuffer())
 );
 
@@ -41,22 +41,22 @@ await db.registerFileBuffer(
 const conn = await db.connect();
 
 const locations: Table<{ location: Utf8 }> = await conn.query(`
-SELECT DISTINCT location
-FROM weather.parquet`);
+SELECT DISTINCT City
+FROM pittsburgh-air-quality.parquet`);
 
 // Create a select element for the locations.
 const select = d3.select(app).append("select");
-for (const location of locations) {
-  select.append("option").text(location.location);
+for (const City of Cities) {
+  select.append("option").text(City.City);
 }
 
 select.on("change", () => {
-  const location = select.property("value");
-  update(location);
+  const City = select.property("value");
+  update(City);
 });
 
 // Update the chart with the first location.
-update("Seattle");
+update("All");
 
 // Add the chart to the DOM.
 app.appendChild(chart.element);
